e2e30f8869eded941cfd998377a63ce92a21f0ff
GoAlek <alek.golaszewski@gmail.com>
2022-04-09 22:39:50 +0200

    added dimmer to Delete

--- src/components/Delete.js
+++ src/components/Delete.js
@@ -18,18 +18,20 @@ export const Delete = (props) => {
   }, [dispatch, id, navigate, userId]);
 
   return ReactDom.createPortal(
-    <div className="ui mini modal active">
-      <div className="header">{`Delete ${userToDelete.name}`}</div>
-      <div className="content">
-        Are you sure you want to delete user?
-      </div>
-      <div className="actions">
-        <Link to="/">
-          <button className="ui black button" type="button">Cancel</button>
-        </Link>
-        <button className="ui red button" type="button" onClick={onConfirm}>
-          Delete
-        </button>
+    <div className="ui page dimmer visible active">
+      <div className="ui mini modal active">
+        <div className="header">{`Delete ${userToDelete.name}`}</div>
+        <div className="content">
+          Are you sure you want to delete user?
+        </div>
+        <div className="actions">
+          <Link to="/">
+            <button className="ui black button" type="button">Cancel</button>
+          </Link>
+          <button className="ui red button" type="button" onClick={onConfirm}>
+            Delete
+          </button>
+        </div>
       </div>
     </div>,
     document.querySelector('#modal'),
